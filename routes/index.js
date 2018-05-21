2a8ea22bceac09aade3c916cebb128b22b80edeb
Daniel <daniel@daniel.linux>
2018-05-21 19:14:30 +0200

    Step123

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Quiz' });
+});
+
+
+// Author page
+router.get('/author',(req,res,next)=>{
+	res.render('author');
+});
+
+module.exports = router;
