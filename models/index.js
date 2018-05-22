7278bac7bd596534f566c51438a78334638effdb
Daniel <daniel@daniel.linux>
2018-05-22 17:55:35 +0200

    Step8

--- models/index.js
+++ models/index.js
@@ -1,4 +1,22 @@
-const quiz= require("./quiz");
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+// To use SQLite data base:
+const sequelize = new Sequelize("sqlite:quiz.sqlite");
+
+// Import the definition of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname, 'quiz'));// Con esto llamamos al quiz definido
 
 
-exports.quiz = quiz;
+// Create tables
+sequelize.sync()
+.then(() => console.log('Data Bases created successfully'))
+.catch(error => {
+    console.log("Error creating the data base tables:", error);
+    process.exit(1);
+});
+
+
+module.exports = sequelize;
