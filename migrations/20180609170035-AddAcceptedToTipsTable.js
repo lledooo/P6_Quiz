5eb902e8f75fb202be53923d69d1b6fbe7ad223f
Daniel <daniel@daniel.linux>
2018-06-10 13:55:44 +0200

    Step21b

---
+++ migrations/20180609170035-AddAcceptedToTipsTable.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    up(queryInterface, Sequelize) {
+        return queryInterface.addColumn(
+            'tips',
+            'accepted',
+            {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false
+            }
+        );
+    },
+
+    down(queryInterface, Sequelize) {
+        return queryInterface.removeColumn('tips', 'accepted');
+    }
+};
+
+
